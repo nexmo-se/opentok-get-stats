05939920de19e60a7f5ac3f6cbbbbf14019725b5
Frans Huang <franssiswanto@gmail.com>
2021-03-25 09:51:54 +0700

    Heroku

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
-var apiKey = "";
-var sessionId = "";
-var token = "";
+var apiKey = "46789364";
+var sessionId = "1_MX40Njc4OTM2NH5-MTYxNjYzOTgyOTQ5OX5uelNweks0VnJGY2ZmZmNVVktUczdTMGt-fg";
+var token = "T1==cGFydG5lcl9pZD00Njc4OTM2NCZzaWc9ZmI5YTNjMmI5ZTNhMDkxMGI1M2IxNzAzYWQzOWJkMmJiY2Y5ZjE0ZTpzZXNzaW9uX2lkPTFfTVg0ME5qYzRPVE0yTkg1LU1UWXhOall6T1RneU9UUTVPWDV1ZWxOd2VrczBWbkpHWTJabVptTlZWa3RVY3pkVE1HdC1mZyZjcmVhdGVfdGltZT0xNjE2NjQwNjY3Jm5vbmNlPTAuMjE4ODI3NjE2OTY0NzQwMyZyb2xlPW1vZGVyYXRvciZleHBpcmVfdGltZT0xNjE2NzI3MDY3JmNvbm5lY3Rpb25fZGF0YT0lN0IlMjJuYW1lJTIyJTNBJTIyRnJhbnMlMjBTaXN3YW50byUyMiUyQyUyMnJvbGUlMjIlM0ElMjJtb2RlcmF0b3IlMjIlN0QmaW5pdGlhbF9sYXlvdXRfY2xhc3NfbGlzdD0=";
 var publisher;
 let runInternal = null;
 
